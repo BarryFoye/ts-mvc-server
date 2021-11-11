b6c759422c64df66cf3a795cb24d885343725f5d
Barry Foye <barry.foye@gmail.com>
2021-11-11 14:32:08 +0000

    added logic for when an env file is used vs none. corrected the second call

--- src/controllers/market_data.ts
+++ src/controllers/market_data.ts
@@ -2,20 +2,21 @@ import axios, { AxiosResponse } from 'axios';
 import { Get, Route, Tags } from 'tsoa';
 import { Coin } from '../@types/app';
 
+const url = process.env.DB_HOST || 'localhost'; // TODO: this should be done at root through a config
 @Route('market_data')
 @Tags('MarketData')
 export default class MarketDataController {
 	@Get('/coin')
 	public async getWeeklySnapshots(): Promise<Coin> {
 		// get all weekly snapshots
-		let result: AxiosResponse = await axios.get(`http://${process.env.DB_HOST}:3100/v1/market_data/coin`);
+		let result: AxiosResponse = await axios.get(`http://${url}:3100/v1/market_data/coin`);
 		return result.data;
 	}
 
 	@Get('/tree_map')
-	public async dataTree(): Promise<Coin> {
+	public async dataTree(): Promise<unknown> {
 		// get all weekly snapshots
-		let result: AxiosResponse = await axios.get(`http://${process.env.DB_HOST}/v1/market_data/tree_map`);
+		let result: AxiosResponse = await axios.get(`http://${url}:3100/v1/market_data/tree_map`);
 		return result.data;
 	}
 }
